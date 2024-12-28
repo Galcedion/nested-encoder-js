91e88a84b9f00a32b69dfb7590327498970ee9b3
Galcedion <galcedion@gmx.de>
2024-12-28 22:48:47 +0100

    adding combined ascii + base<x> improving code structure

--- nested-encoder.js
+++ nested-encoder.js
@@ -2,6 +2,7 @@ function nestedEncoder(plainString = false, pattern = false) {
 	if(!plainString || typeof plainString === 'undefined' || !pattern || typeof pattern === 'undefined') {
 		return nestedEncoderOptions();
 	} else {
+		pattern = pattern.toLowerCase();
 		if(typeof pattern !== 'object') {
 			pattern = pattern.split(',');
 		}
@@ -13,8 +14,9 @@ function nestedEncoderOptions() {
 	var options = {};
 	options['parameters'] = ['plainString', 'pattern'];
 	options['encodings'] = {
-		'ascii': 'ASCII',
+		'ascii': 'ASCII encoding',
 		'base<x>': 'different numbering systems where x is the base of the numbering system; x can be one of: 1, 2, 3, 4, 5, 6, 7, 8, 12, 16, 20, 64',
+		'abase<x>': 'building onto base (see base<x>) the string is ASCII encoded (see ascii) before the base encoding',
 		'binary': 'see base2',
 		'duodec': 'see base12',
 		'hex': 'see base16',
@@ -33,64 +35,36 @@ function nestedEncoderOptions() {
 }
 
 function nestedEncoderEncode(plainString, pattern) {
+	var alias = {'binary': 'base2', 'duodec': 'base12', 'hex': 'base16', 'oct': 'base8', 'pental': 'base5', 'quaternary': 'base4', 'senary': 'base6', 'septenary': 'base7', 'trinary': 'base3', 'unary': 'base1', 'vigesimal': 'base20'};
 	var encodedString = plainString;
 	for(let i = 0; i < pattern.length; i++) {
+		if(Object.keys(alias).indexOf(pattern[i]) != -1)
+			pattern[i] = (alias[pattern[i]]);
+		if(pattern[i].startsWith('abase') || pattern[i].startsWith('base')) {
+			let base = 0;
+			if(pattern[i][0] == 'a') {
+				encodedString = nestedEncoderEncodeAscii(encodedString);
+				base = pattern[i].substring(5);
+			} else {
+				base = pattern[i].substring(4);
+			}
+			if(base == 1)
+				encodedString = nestedEncoderEncodeUnary(encodedString);
+			else if(base == 64)
+				encodedString = nestedEncoderEncodeBase64(encodedString);
+			else
+				encodedString = nestedEncoderEncodeMultiBase(encodedString, base);
+			continue;
+		}
 		switch(pattern[i]) {
 			case 'ascii':
 				encodedString = nestedEncoderEncodeAscii(encodedString);
 				break
-			case 'base64':
-				encodedString = nestedEncoderEncodeBase64(encodedString);
-				break;
-			case 'binary':
-			case 'base2':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 2);
-				break;
-			case 'duodec':
-			case 'base12':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 12);
-				break;
-			case 'hex':
-			case 'base16':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 16);
-				break;
 			case 'html':
 				encodedString = nestedEncoderEncodeHTML(encodedString);
 				break;
-			case 'oct':
-			case 'base8':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 8);
-				break;
-			case 'pental':
-			case 'base5':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 5);
-				break;
-			case 'quaternary':
-			case 'base4':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 4);
-				break;
-			case 'senary':
-			case 'base6':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 6);
-				break;
-			case 'septenary':
-			case 'base7':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 7);
-				break;
-			case 'trinary':
-			case 'base3':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 3);
-				break;
-			case 'unary':
-			case 'base1':
-				encodedString = nestedEncoderEncodeUnary(encodedString);
-				break;
 			case 'unicode':
 				encodedString = nestedEncoderEncodeUnicode(encodedString);
-				break;
-			case 'vigesimal':
-			case 'base20':
-				encodedString = nestedEncoderEncodeMultiBase(encodedString, 20);
 				break;
 		}
 	}
